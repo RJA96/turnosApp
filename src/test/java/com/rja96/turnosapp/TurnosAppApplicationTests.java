f583f4bf2aee59b4b7ee00257c1dbb930c8c0067
Rodrigo Aguilar <rodrigoaguilar96@gmail.com>
2021-08-28 02:26:44 -0300

    Initial commit

---
+++ src/test/java/com/rja96/turnosapp/TurnosAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rja96.turnosapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurnosAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
